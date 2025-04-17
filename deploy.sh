5f7d06dfbe9adb27fa0af439840f239b083122b3
Siddhart <siddhartssg@gmail.com>
2025-04-17 22:20:45 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ git pull origin main
 # Restart docker-compose (adjust if using a different dir or file)
 cd /root/waltid/prod/docker-compose
 docker compose down
-docker compose up -d --build
+docker compose up -d
 
 echo "✅ Deployment complete."
+#test
